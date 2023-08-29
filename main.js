8041b584a8ababed125eeb54641bfd192be3b9ce
Heiner Landero <heinerlandero@gmail.com>
2023-08-29 09:45:00 -0500

    29/08/2023 se hace una refactorizacion del condigo para insertar un swiper

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
-const dropdowns = document.querySelectorAll('.dropdown');
-  
-dropdowns.forEach(dropdown => {
-    const select = dropdown.querySelector('.btn-select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelector('.menu li');
-    const selected = dropdown.querySelector('.selected');
-
-
-select.addEventListener('click', ()=>{
-    select.classList.toggle('select-clicked');
-    caret.classList.toggle('caret-rotate');
-    menu.classList.toggle('menu-open');
-})
-
-options.forEach(option =>{
-    option.addEventListener('click' , () =>{
-        selected.innerText = option.innerText;
-        select.classList.remove('select-clicked')
-        caret.classList.remove('caret-rotate');
-        menu.classList.remove('menu-open');
-
-        options.forEach(option =>{
-            option.classList.remove('active');
-        });
-            option.classList.add('active');
-    });
-});
-});
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
